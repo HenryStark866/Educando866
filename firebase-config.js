af912cd6593c97b729f54f7fd8362aea9c6856cf
HenryStark866 <henrystark866@gmail.com>
2026-03-19 17:34:48 -0500

    chore: full firebase configuration and finalization

--- firebase-config.js
+++ firebase-config.js
@@ -4,8 +4,13 @@ import { getAuth, signInAnonymously } from "https://www.gstatic.com/firebasejs/1
 
 const firebaseConfig = {
     apiKey: "AIzaSyBOLlMtZB9vfdbWo3PlABfTru19A2Id2I4",
+    authDomain: "prepudea-platform.firebaseapp.com",
+    databaseURL: "https://prepudea-platform-default-rtdb.firebaseio.com",
     projectId: "prepudea-platform",
-    appId: "1:30596061507:web:7dd5dbbad3ededc5f6f7f6"
+    storageBucket: "prepudea-platform.firebasestorage.app",
+    messagingSenderId: "30596061507",
+    appId: "1:30596061507:web:7dd5dbbad3ededc5f6f7f6",
+    measurementId: "G-Y19TSL20GR"
 };
 
 // Initialize Firebase
